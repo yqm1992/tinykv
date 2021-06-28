230100e29a021946806c32a29e91502c236ebced
yangqiuming <yqm1992ofq@qq.com>
2021-06-28 15:04:27 +0800

    add implementation of Schedule()

--- scheduler/server/schedulers/balance_region.go
+++ scheduler/server/schedulers/balance_region.go
@@ -14,10 +14,13 @@
 package schedulers
 
 import (
+	"github.com/pingcap-incubator/tinykv/log"
 	"github.com/pingcap-incubator/tinykv/scheduler/server/core"
 	"github.com/pingcap-incubator/tinykv/scheduler/server/schedule"
 	"github.com/pingcap-incubator/tinykv/scheduler/server/schedule/operator"
 	"github.com/pingcap-incubator/tinykv/scheduler/server/schedule/opt"
+	"go.uber.org/zap"
+	"sort"
 )
 
 func init() {
@@ -78,5 +81,66 @@ func (s *balanceRegionScheduler) IsScheduleAllowed(cluster opt.Cluster) bool {
 func (s *balanceRegionScheduler) Schedule(cluster opt.Cluster) *operator.Operator {
 	// Your Code Here (3C).
 
+	storeArray := cluster.GetStores()
+	suitableStores := make([]*core.StoreInfo, 0)
+	maxStoreDownTime := cluster.GetMaxStoreDownTime()
+	for _, curStore := range storeArray {
+		if curStore.IsUp() && curStore.DownTime() <= maxStoreDownTime {
+			suitableStores = append(suitableStores, curStore)
+		}
+	}
+	if len(suitableStores) < 2 {
+		return nil
+	}
+	// sort by desc
+	sort.Slice(suitableStores, func(i, j int) bool {
+		return suitableStores[i].GetRegionSize() > suitableStores[j].GetRegionSize()
+	})
+	targetStore := suitableStores[len(suitableStores)-1]
+	getRegionFuncs := make([]func(storeID uint64, opts ...core.RegionOption) *core.RegionInfo, 3)
+	getRegionFuncs[0] = cluster.RandPendingRegion
+	getRegionFuncs[1] = cluster.RandFollowerRegion
+	getRegionFuncs[2] = cluster.RandLeaderRegion
+	for _, curStore := range suitableStores {
+		// src and dst store can not be the same one
+		if curStore == targetStore {
+			break
+		}
+		srcStore := curStore
+		srcID := srcStore.GetID()
+		for j := 0; j < len(getRegionFuncs); j++ {
+			getRegionFunc := getRegionFuncs[j]
+			for  k := 0; k < balanceRegionRetryLimit; k++ {
+				srcRegion := getRegionFunc(srcID)
+				if srcRegion != nil {
+					if op := s.createOperator(cluster, srcRegion, srcStore, targetStore); op != nil {
+						return op
+					}
+				}
+			}
+		}
+		log.Debug("no operator created for selected stores", zap.String("scheduler", s.GetName()), zap.Uint64("source", srcID))
+	}
+	return nil
+}
+
+// createOperator creates the operator according to the source and target store.
+// If the difference between the two stores is tolerable, then
+// no new operator need to be created, otherwise create an operator that moves
+// region from the source store to the target store
+func (s *balanceRegionScheduler) createOperator(cluster opt.Cluster, region *core.RegionInfo, source, target *core.StoreInfo) *operator.Operator {
+	if source.GetRegionSize() - target.GetRegionSize() < 2*region.GetApproximateSize() {
+		return nil
+	}
+	newPeer, err := cluster.AllocPeer(target.GetID())
+	if err != nil {
+		return nil
+	}
+	sourceID := source.GetID()
+	targetID := target.GetID()
+	newPeerId := newPeer.GetId()
+	if op, err := operator.CreateMovePeerOperator("balance-region", cluster, region, operator.OpBalance, sourceID, targetID, newPeerId); err == nil {
+		return op
+	}
 	return nil
 }
