c551acfab0b15facaff8aa9b3340d85088c2830b
yangqiuming <yqm1992ofq@qq.com>
2021-06-29 17:11:05 +0800

    remove the unused code

--- scheduler/server/schedulers/balance_region.go
+++ scheduler/server/schedulers/balance_region.go
@@ -81,66 +81,6 @@ func (s *balanceRegionScheduler) IsScheduleAllowed(cluster opt.Cluster) bool {
 	return s.opController.OperatorCount(operator.OpRegion) < cluster.GetRegionScheduleLimit()
 }
 
-func (s *balanceRegionScheduler) ScheduleOld(cluster opt.Cluster) *operator.Operator {
-	// Your Code Here (3C).
-
-	stores := cluster.GetStores()
-	sources := make([]*core.StoreInfo, 0)
-	targets := make([]*core.StoreInfo, 0)
-	maxStoreDownTime := cluster.GetMaxStoreDownTime()
-	for _, curStore := range stores {
-		if curStore.IsUp() && curStore.DownTime() <= maxStoreDownTime {
-			sources = append(sources, curStore)
-			targets = append(targets, curStore)
-		}
-	}
-	if len(sources) < 2 {
-		return nil
-	}
-	// sort by desc
-	sort.Slice(sources, func(i, j int) bool {
-		return sources[i].GetRegionSize() > sources[j].GetRegionSize()
-	})
-	sort.Slice(targets, func(i, j int) bool {
-		return targets[i].GetRegionSize() < targets[j].GetRegionSize()
-	})
-	//targetStore := suitableStores[len(suitableStores)-1]
-	getRegionFuncs := make([]func(storeID uint64, opts ...core.RegionOption) *core.RegionInfo, 3)
-	getRegionFuncs[0] = cluster.RandPendingRegion
-	getRegionFuncs[1] = cluster.RandFollowerRegion
-	getRegionFuncs[2] = cluster.RandLeaderRegion
-	for index, curStore := range sources {
-		// src and dst store can not be the same one
-		if index == len(sources) - 1 {
-			break
-		}
-		srcStore := curStore
-		srcID := srcStore.GetID()
-		for j := 0; j < len(getRegionFuncs); j++ {
-			getRegionFunc := getRegionFuncs[j]
-			for  k := 0; k < balanceRegionRetryLimit; k++ {
-				srcRegion := getRegionFunc(srcID)
-				if srcRegion != nil {
-					srcRegionStores := srcRegion.GetStoreIds()
-					for _, targetStore := range targets {
-						if targetStore.GetRegionSize() >= srcStore.GetRegionSize() {
-							break
-						}
-						if _, ok := srcRegionStores[targetStore.GetID()]; ok {
-							continue
-						}
-						if op := s.createOperator(cluster, srcRegion, srcStore, targetStore); op != nil {
-							return op
-						}
-					}
-				}
-			}
-		}
-		log.Debug("no operator created for selected stores", zap.String("scheduler", s.GetName()), zap.Uint64("source", srcID))
-	}
-	return nil
-}
-
 func (s *balanceRegionScheduler) Schedule(cluster opt.Cluster) *operator.Operator {
 	// Your Code Here (3C).
 
