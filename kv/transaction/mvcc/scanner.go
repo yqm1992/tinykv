c988e4cf7a264012669c714b053284ec9f6263c3
yangqiuming <yqm1992ofq@qq.com>
2021-07-13 10:15:50 +0800

    add implementation of scanner

--- kv/transaction/mvcc/scanner.go
+++ kv/transaction/mvcc/scanner.go
@@ -1,24 +1,72 @@
 package mvcc
 
+import (
+	"bytes"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
+)
+
 // Scanner is used for reading multiple sequential key/value pairs from the storage layer. It is aware of the implementation
 // of the storage layer and returns results suitable for users.
 // Invariant: either the scanner is finished and cannot be used, or it is ready to return a value immediately.
 type Scanner struct {
 	// Your Data Here (4C).
+	txn *MvccTxn
+	iter engine_util.DBIterator
 }
 
 // NewScanner creates a new scanner ready to read from the snapshot in txn.
 func NewScanner(startKey []byte, txn *MvccTxn) *Scanner {
 	// Your Code Here (4C).
-	return nil
+	iter := txn.Reader.IterCF(engine_util.CfWrite)
+	iter.Seek(keyMaxVersion(startKey))
+	return &Scanner{txn: txn, iter: iter}
 }
 
 func (scan *Scanner) Close() {
 	// Your Code Here (4C).
+	scan.iter.Close()
+	scan.txn = nil
 }
 
 // Next returns the next key/value pair from the scanner. If the scanner is exhausted, then it will return `nil, nil, nil`.
 func (scan *Scanner) Next() ([]byte, []byte, error) {
 	// Your Code Here (4C).
-	return nil, nil, nil
+	var val []byte
+	var err error
+	var write *Write
+	var foundKey []byte
+	var foundVal []byte
+
+	iter := scan.iter
+	for ;iter.Valid(); iter.Next() {
+		item := iter.Item()
+		// is this write is visible to txn
+		if writeTs := decodeTimestamp(item.Key()); writeTs > scan.txn.StartTS {
+			continue
+		}
+		if val, err = item.Value(); err != nil {
+			return nil, nil, err
+		}
+		if write, err = ParseWrite(val); err != nil {
+			return nil, nil, err
+		}
+		// the deleted value is also visible to transaction
+		if write.Kind != WriteKindPut && write.Kind != WriteKindDelete {
+			continue
+		}
+		foundKey = DecodeUserKey(item.Key())
+		if write.Kind == WriteKindPut {
+			if val, err = scan.txn.Reader.GetCF(engine_util.CfDefault, EncodeKey(foundKey, write.StartTS)); err != nil {
+				return nil, nil, err
+			}
+			foundVal = val
+		}
+		// skip foundKey's old versions
+		iter.Seek(keyMinVersion(foundKey))
+		if iter.Valid() && bytes.Compare(DecodeUserKey(iter.Item().Key()), foundKey) == 0 {
+			iter.Next()
+		}
+		break
+	}
+	return foundKey, foundVal, nil
 }
