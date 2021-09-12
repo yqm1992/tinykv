16e93accd464303db07d283161833f4a722389bc
yangqiuming <yqm1992ofq@qq.com>
2021-09-12 09:37:14 +0800

    remove unused codes

--- kv/transaction/mvcc/scanner.go
+++ kv/transaction/mvcc/scanner.go
@@ -140,56 +140,6 @@ func compareIterWriteLock(writeIter engine_util.DBIterator, lockIter engine_util
 }
 
 // Next returns the next key/value pair from the scanner. If the scanner is exhausted, then it will return `nil, nil, nil`.
-func (scan *Scanner) OldNext() ([]byte, []byte, error) {
-	// Your Code Here (4C).
-	var val []byte
-	var err error
-	var write *Write
-	var foundKey []byte
-	var foundVal []byte
-
-	writeIter := scan.writeIter
-	for ;writeIter.Valid();  {
-		item := writeIter.Item()
-		// is this write is visible to txn
-		if writeTs := decodeTimestamp(item.Key()); writeTs > scan.txn.StartTS {
-			writeIter.Next()
-			continue
-		}
-		if val, err = item.Value(); err != nil {
-			return nil, nil, err
-		}
-		if write, err = ParseWrite(val); err != nil {
-			return nil, nil, err
-		}
-		// the deleted value is also visible to transaction
-		if write.Kind != WriteKindPut && write.Kind != WriteKindDelete {
-			writeIter.Next()
-			continue
-		}
-		foundKey = DecodeUserKey(item.Key())
-		if write.Kind == WriteKindPut {
-			if val, err = scan.txn.Reader.GetCF(engine_util.CfDefault, EncodeKey(foundKey, write.StartTS)); err != nil {
-				log.Fatal(err)
-			}
-			foundVal = val
-		}
-		// skip foundKey's old versions
-		writeIter.Seek(keyMinVersion(foundKey))
-		if writeIter.Valid() && bytes.Compare(DecodeUserKey(writeIter.Item().Key()), foundKey) == 0 {
-			writeIter.Next()
-		}
-		if write.Kind == WriteKindDelete {
-			// The found write is delete, skips it
-			foundKey = nil
-			continue
-		}
-		break
-	}
-	return foundKey, foundVal, nil
-}
-
-// Next returns the next key/value pair from the scanner. If the scanner is exhausted, then it will return `nil, nil, nil`.
 func (scan *Scanner) Next() ([]byte, []byte, error) {
 	// Your Code Here (4C).
 	writeIter := scan.writeIter
